3f23aef8e1a19f505580e610a08e92f9b704ab26
Vyas Ramankulangara <vyas01890@outlook.com>
2021-06-30 17:13:09 -0500

    algolia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.css" />
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@algolia/algoliasearch-netlify-frontend@1/dist/algoliasearchNetlify.js"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>);
 }
 export default MyApp;
