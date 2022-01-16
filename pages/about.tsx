2b73dd135180471b26d6b7f0e9d87cbc99f2ae2d
Vyas Ramankulangara <vyas01890@outlook.com>
2022-01-16 14:30:46 -0600

    Added about page

---
+++ pages/about.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+
+export default function About() {
+
+  return (
+    <>
+        <Head>
+          <title>About</title>
+          <meta name="description" content="About" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <div>
+          <h1>About Me</h1>
+        </div>
+    </>
+  );
+}
