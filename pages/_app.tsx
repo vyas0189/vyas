c3aba66405594c4857d4214174e684a309bca84d
Vyas Ramankulangara <vyas01890@outlook.com>
2022-01-25 15:02:01 -0600

    added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps } from 'next/app';
-import '../styles/index.css'
+import '../styles/index.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script id='ga-analytics' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+      </Script>
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
-
