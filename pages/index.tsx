648dcc22aaa7eefcc48d93ab64bd8ce16e8eaa71
Vyas Ramankulangara <vyas01890@outlook.com>
2022-01-25 15:07:15 -0600

    clean up 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
 import Link from 'next/link';
 
 function Home() {
-  return <>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/about">
-          <a>About Us</a>
-        </Link>
-      </li>
-    </ul>
-    <div>
-      <p className='text-3xl'> Vyas Ramankulangara </p>
-    </div>
-  </>;
-
+  return (
+    <>
+      <ul>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href='/about'>
+            <a>About Us</a>
+          </Link>
+        </li>
+      </ul>
+      <div>
+        <p className='text-3xl'> Vyas Ramankulangara </p>
+      </div>
+    </>
+  );
 }
 
 export default Home;
