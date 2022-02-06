ec784c26b9f3b4a9a142514825eeac878d45154a
Vyas Ramankulangara <vyas01890@outlook.com>
2022-02-05 22:49:55 -0600

    Link home

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,19 +1,24 @@
 import Head from 'next/head';
-
+import Link from "next/link";
 
 export default function About() {
 
   return (
     <>
-        <Head>
-          <title>About</title>
-          <meta name="description" content="About" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
+      <Head>
+        <title>About</title>
+        <meta name="description" content="About" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-        <div>
-          <h1>About Me</h1>
-        </div>
+      <div>
+        <h1>About Me</h1>
+      </div>
+
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+
     </>
   );
 }
