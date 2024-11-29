ef7353b21171796c3e10198a5fcf5dcd9b00838e
Vyas Ramankulangara <vyas01890@outlook.com>
2024-11-29 12:04:47 -0600

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
