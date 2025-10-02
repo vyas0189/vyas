7a49ccdbcd586c4f828855f2703b52fc2f840306
Vyas Ramankulangara <vyas01890@outlook.com>
2025-10-01 23:34:48 -0500

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,13 +4,15 @@ export default defineConfig({
 	testDir: './tests/e2e',
 	fullyParallel: true,
 	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 1 : 0,
+	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 4 : undefined,
 	reporter: 'html',
+	timeout: 30000,
 	use: {
 		baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:4321',
 		trace: 'on-first-retry',
 		screenshot: 'only-on-failure',
+		navigationTimeout: 15000,
 	},
 	projects: process.env.CI
 		? [
